e4725173b9e6cdc6bbd8fd68568d01b6c02ceeb5
euldong.kim <euldong.kim@linecorp.com>
2019-02-17 17:10:05 +0900

    Servlet listener test

---
+++ servlet-test/src/main/java/com/tistory/windingroad/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.tistory.windingroad;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+  @Override
+  public void init() throws ServletException {
+    System.out.println("init");
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    System.out.println("doGet");
+    resp.getWriter().println("<html>");
+    resp.getWriter().println("<body>");
+    resp.getWriter().println("Hello Servlet");
+    resp.getWriter().println("</body>");
+    resp.getWriter().println("</html>");
+  }
+
+  @Override
+  public void destroy() {
+    System.out.println("destroy");
+  }
+}
