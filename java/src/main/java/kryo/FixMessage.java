4753a93abfa306900da851943bf39330932937fa
WindingRoadss <ked9181@naver.com>
2019-07-17 11:49:07 +0900

    step verifier test

--- java/src/main/java/kryo/FixMessage.java
+++ java/src/main/java/kryo/FixMessage.java
@@ -1,22 +1,24 @@
 package kryo;
 
-import java.io.IOException;
-
-import sun.plugin2.message.Conversation;
-import sun.plugin2.message.Message;
-import sun.plugin2.message.Serializer;
-
-public class FixMessage extends Message {
-
-    public FixMessage(int i, Conversation conversation) {
-        super(i, conversation);
-    }
-
-    public void writeFields(Serializer serializer) throws IOException {
-
-    }
-
-    public void readFields(Serializer serializer) throws IOException {
-
-    }
-}
+//import java.io.IOException;
+//
+//// can be operated in java 8
+//
+//import sun.plugin2.message.Conversation;
+//import sun.plugin2.message.Message;
+//import sun.plugin2.message.Serializer;
+//
+//public class FixMessage extends Message {
+//
+//    public FixMessage(int i, Conversation conversation) {
+//        super(i, conversation);
+//    }
+//
+//    public void writeFields(Serializer serializer) throws IOException {
+//
+//    }
+//
+//    public void readFields(Serializer serializer) throws IOException {
+//
+//    }
+//}
--- java/src/main/java/reactor/stepverifier/ExampleStepVerifier.java
+++ java/src/main/java/reactor/stepverifier/ExampleStepVerifier.java
@@ -1,4 +1,46 @@
 package reactor.stepverifier;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 public class ExampleStepVerifier {
+
+    @Test
+    public void test() {
+        List<String> list = new ArrayList<>();
+        list.add("foo");
+        list.add("bar");
+        Flux<String> flux = Flux.fromIterable(list);
+        StepVerifier.create(flux)
+                    .expectNext("foo", "bar")
+                    .expectComplete()
+                    .verify();
+    }
+
+    @Test
+    public void test_exception() {
+        StepVerifier.create(createException(true))
+                    .expectErrorMatches(throwable -> throwable instanceof RuntimeException)
+                    .verify();
+
+        StepVerifier.create(createException(false))
+                    .expectNext("foo", "bar")
+                    .verifyComplete();
+    }
+
+    private Flux createException(boolean isError) throws RuntimeException {
+        Flux<String> flux = Flux.fromIterable(List.of("foo", "bar"));
+
+        if(isError) {
+            return Flux.error(new RuntimeException("exception test"));
+        }
+
+        return flux;
+    }
 }
